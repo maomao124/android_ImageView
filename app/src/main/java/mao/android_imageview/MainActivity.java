548ae2f0b945a2935a4b2d942d274a858b6df8f8
mao <1296193245@qq.com>
2022-09-20 10:38:49 +0800

    android_ImageView

--- app/src/main/java/mao/android_imageview/MainActivity.java
+++ app/src/main/java/mao/android_imageview/MainActivity.java
@@ -2,7 +2,9 @@ package mao.android_imageview;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -12,5 +14,13 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.button1).setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                startActivity(new Intent(MainActivity.this, MainActivity2.class));
+            }
+        });
     }
 }
