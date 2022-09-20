da85baaaa4342a58b2605d447c8757e5c9c8d11a
mao <1296193245@qq.com>
2022-09-20 10:46:56 +0800

    android_ImageView

--- app/src/main/java/mao/android_imageview/MainActivity.java
+++ app/src/main/java/mao/android_imageview/MainActivity.java
@@ -22,5 +22,69 @@ public class MainActivity extends AppCompatActivity
                 startActivity(new Intent(MainActivity.this, MainActivity2.class));
             }
         });
+        findViewById(R.id.button1).setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                startActivity(new Intent(MainActivity.this, MainActivity2.class));
+            }
+        });
+        findViewById(R.id.button2).setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                startActivity(new Intent(MainActivity.this, MainActivity3.class));
+            }
+        });
+        findViewById(R.id.button3).setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                startActivity(new Intent(MainActivity.this, MainActivity4.class));
+            }
+        });
+        findViewById(R.id.button4).setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                startActivity(new Intent(MainActivity.this, MainActivity5.class));
+            }
+        });
+        findViewById(R.id.button5).setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                startActivity(new Intent(MainActivity.this, MainActivity6.class));
+            }
+        });
+        findViewById(R.id.button6).setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                startActivity(new Intent(MainActivity.this, MainActivity7.class));
+            }
+        });
+        findViewById(R.id.button7).setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                startActivity(new Intent(MainActivity.this, MainActivity8.class));
+            }
+        });
+        findViewById(R.id.button8).setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                startActivity(new Intent(MainActivity.this, MainActivity9.class));
+            }
+        });
     }
 }
